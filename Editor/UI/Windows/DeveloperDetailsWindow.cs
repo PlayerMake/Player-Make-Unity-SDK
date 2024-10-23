375d3adbb88a39476d98650df6aafac8d23f6b88
Max Andreassen <andreassenmax@gmail.com>
2024-10-23 15:40:09 +0100

    feat: adds developer details window

--- Editor/UI/Windows/DeveloperDetailsWindow.cs
+++ Editor/UI/Windows/DeveloperDetailsWindow.cs
@@ -5,6 +5,8 @@ namespace PlayerMake.Editor.UI.Windows
 {
     public class DeveloperDetailsWindow : EditorWindow
     {
+        PlayerMakeSettings settings;
+
         [MenuItem("Tools/Player Make", false, 0)]
         public static void Generate()
         {
@@ -14,16 +16,16 @@ namespace PlayerMake.Editor.UI.Windows
 
         private void OnEnable()
         {
-            var sdkSettings = Resources.Load<PlayerMakeSettings>("PlayerMakeSettings");
+            settings = Resources.Load<PlayerMakeSettings>("PlayerMakeSettings");
 
-            if (sdkSettings == null)
+            if (settings == null)
             {
-                sdkSettings = CreateInstance<PlayerMakeSettings>();
+                settings = CreateInstance<PlayerMakeSettings>();
 
                 EnsureResourcePathExists();
 
-                AssetDatabase.CreateAsset(sdkSettings, "Assets/Player Make/Resources/PlayerMakeSettings.asset");
-                EditorUtility.SetDirty(sdkSettings);
+                AssetDatabase.CreateAsset(settings, "Assets/Player Make/Resources/PlayerMakeSettings.asset");
+                EditorUtility.SetDirty(settings);
                 AssetDatabase.SaveAssets();
                 AssetDatabase.Refresh();
             }
@@ -40,9 +42,58 @@ namespace PlayerMake.Editor.UI.Windows
 
         private void OnGUI()
         {
+            GUILayout.Space(10);
+
             using (new GUILayout.HorizontalScope())
             {
-                GUILayout.Label($"Welcome to the Player Make SDK!");
+                GUILayout.Label($"Welcome to the Player Make SDK!", new GUIStyle()
+                {
+                    fontStyle = FontStyle.Bold,
+                    normal = new GUIStyleState()
+                    {
+                        textColor = Color.white
+                    },
+                    margin = new RectOffset(5, 0, 0, 0),
+                    alignment = TextAnchor.MiddleCenter,
+                });
+            }
+
+            GUILayout.Space(10);
+
+            GUILayout.Label("Project Settings", new GUIStyle()
+            {
+                fontStyle = FontStyle.Bold,
+                normal = new GUIStyleState() {
+                    textColor = Color.white
+                },
+                margin = new RectOffset(5, 0, 0, 0),
+            });
+            settings.ProjectId = EditorGUILayout.TextField("Project ID", settings.ProjectId);
+
+            GUILayout.Space(5);
+
+            GUILayout.Label("Auth Settings", new GUIStyle()
+            {
+                fontStyle = FontStyle.Bold,
+                normal = new GUIStyleState()
+                {
+                    textColor = Color.white
+                },
+                margin = new RectOffset(5, 0, 0, 0),
+            });
+
+            settings.ApiProxyUrl = EditorGUILayout.TextField("API Proxy Url", settings.ApiProxyUrl);
+            settings.ApiKey = EditorGUILayout.TextField("API Key", settings.ApiKey);
+
+            using (new GUILayout.HorizontalScope(new GUIStyle()
+            {
+                margin = new RectOffset(5, 5, 5, 0)
+            }))
+            {
+                EditorGUILayout.HelpBox(
+                    "Setting the API key field will cause a security risk for your application, as it means that your API key can discovered in your build. We suggest setting the proxy url instead. See our docs for details.",
+                    MessageType.Info
+                );
             }
         }
     }
